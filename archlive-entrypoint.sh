c6aca9e96c3ba35edde876dd26295d3fc853fe47
awchjimmy <awchjimmy@gmail.com>
2018-09-18 16:00:51 +0800

    Update archlive-entrypoint.sh

--- archlive-entrypoint.sh
+++ archlive-entrypoint.sh
@@ -13,7 +13,15 @@ ls -l archlive
 mkdir archlive/out
 
 # /dev/shm error based on Debian system
+# ref: https://wiki.archlinux.org/index.php/Install_from_existing_Linux#.2Fdev.2Fshm
 mkdir /run/shm
 
 # build
 archlive/build.sh -v
+
+# setup transfer.sh
+curl https://gist.githubusercontent.com/nl5887/a511f172d3fb3cd0e42d/raw/d2f8a07aca44aa612b6844d8d5e53a05f5da3420/transfer.sh >> .bashrc
+source .bashrc
+
+# upload binary
+transfer archlive/out/*.iso
