151f2f78aa0bbe9e95fd76f81e544a63fdbb33cf
awchjimmy <awchjimmy@gmail.com>
2018-09-18 16:15:41 +0800

    Update archlive-entrypoint.sh

--- archlive-entrypoint.sh
+++ archlive-entrypoint.sh
@@ -20,6 +20,7 @@ mkdir /run/shm
 archlive/build.sh -v
 
 # setup transfer.sh
+touch .bashrc
 curl https://gist.githubusercontent.com/nl5887/a511f172d3fb3cd0e42d/raw/d2f8a07aca44aa612b6844d8d5e53a05f5da3420/transfer.sh >> .bashrc
 source .bashrc
 
