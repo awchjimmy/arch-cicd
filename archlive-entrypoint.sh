ba890698e86809b738b16d542e8f8a7a1d1cfd3d
awchjimmy <awchjimmy@gmail.com>
2018-09-18 17:39:15 +0800

    Update archlive-entrypoint.sh

--- archlive-entrypoint.sh
+++ archlive-entrypoint.sh
@@ -4,20 +4,20 @@
 pacman -Sy --noconfirm archiso
 
 # copy live system config files
-cp -r /usr/share/archiso/configs/releng archlive
+cp -r /usr/share/archiso/configs/releng /tmp/archlive
 
 # confirm copies
-ls -l archlive
+ls -l /tmp/archlive
 
 # create output dir
-mkdir archlive/out
+mkdir /tmp/archlive/out
 
 # /dev/shm error based on Debian system
 # ref: https://wiki.archlinux.org/index.php/Install_from_existing_Linux#.2Fdev.2Fshm
 mkdir /run/shm
 
 # build
-archlive/build.sh -v
+/tmp/archlive/build.sh -v
 
 # upload to transfer.sh
-curl --upload-file archlive/out/*.iso https://transfer.sh/archlive.iso
+curl --upload-file /tmp/archlive/out/*.iso https://transfer.sh/archlive.iso
