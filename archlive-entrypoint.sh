f7f63f0be643053157db270ff6e7b58f8e2731f0
awchjimmy <awchjimmy@gmail.com>
2018-09-22 09:11:27 +0800

    Remove output dir and comment out find iso command.

--- archlive-entrypoint.sh
+++ archlive-entrypoint.sh
@@ -9,9 +9,6 @@ cp -r /usr/share/archiso/configs/releng /tmp/archlive
 # confirm copies
 ls -l /tmp/archlive
 
-# create output dir
-mkdir /tmp/archlive/out
-
 # /dev/shm error based on Debian system
 # ref: https://wiki.archlinux.org/index.php/Install_from_existing_Linux#.2Fdev.2Fshm
 mkdir /run/shm
@@ -20,8 +17,8 @@ mkdir /run/shm
 /tmp/archlive/build.sh -v
 
 # can't afford 10 mins building a bug...
-echo "start to find .iso"
-find . -type f -name "*.iso"
+# echo "start to find .iso"
+# find . -type f -name "*.iso"
 
 # upload to transfer.sh
 curl --upload-file ./out/*.iso https://transfer.sh/archlive.iso
